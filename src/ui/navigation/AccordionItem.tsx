8eab6c74839a288b471674b3205c50d571024407
Lukasz <7lukasz8@gmail.com>
2023-11-25 22:42:54 +0100

    accordion item

--- src/ui/navigation/AccordionItem.tsx
+++ src/ui/navigation/AccordionItem.tsx
@@ -5,17 +5,20 @@ import { NavLink } from "react-router-dom";
 interface AccordionItemProps {
   onCloseNav: () => void;
   delay: number
+  title: string
+  icon: React.ReactNode
+  to: string
 }
 
-export default function AccordionItem({ onCloseNav, delay }: AccordionItemProps) {
+export default function AccordionItem({ onCloseNav, delay, title, icon, to }: AccordionItemProps) {
   return (
-    <motion.div transition={{delay: 2}}>
+    <motion.div transition={{delay: 2}} className="w-full">
       <NavLink
-        className="origin-top text-bgDark dark:text-bgWhite1"
-        to=""
+        className="flex gap-2 justify-start items-center origin-top text-bgDark dark:text-bgWhite1 px-4 py-3 rounded-lg my-1 bg-white hover:bg-bgWhite1Hover dark:bg-bgDark1 dark:hover:bg-bgDark1Hover transition-colors duration-500"
+        to={to}
         onClick={onCloseNav}
       >
-        AccordionItem
+        <span className="">{icon}</span> {title}
       </NavLink>
     </motion.div>
   );
