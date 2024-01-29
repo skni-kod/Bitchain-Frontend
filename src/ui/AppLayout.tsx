73523172a3b78f50c9c113d40298442cdc7e908b
Lukasz <7lukasz8@gmail.com>
2024-01-29 23:31:39 +0100

    app layout changed

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -6,7 +6,7 @@ export default function AppLayout() {
   return (
     <div id="app" className="grid grid-rows-layout absolute h-full w-full top-0 left-0 overflow-x-hidden bg-white dark:bg-bgDark">
       <Navigation />
-      <div className="overflow-auto">
+      <div className="overflow-x-auto h-full">
         <Outlet />
       </div>
     </div>
