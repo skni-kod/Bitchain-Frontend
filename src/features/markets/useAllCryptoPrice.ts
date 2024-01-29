bfce09381bc075d37bb833a2667cff6e95cd78ba
Lukasz <7lukasz8@gmail.com>
2024-01-29 23:32:04 +0100

    markets cards

--- src/features/markets/useAllCryptoPrice.ts
+++ src/features/markets/useAllCryptoPrice.ts
@@ -2,10 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { getCryptoPrice } from "../../services/apiCoinCap";
 
 export function useAllCryptoPrice(limit?: number, offset?: number) {
-  const { data, isSuccess } = useQuery({
+  const { data, isSuccess, refetch } = useQuery({
     queryFn: () => getCryptoPrice(limit, offset),
     queryKey: ["cryptoPrice"],
+    // enabled: false,
   });
 
-  return { data, isSuccess };
+  return { data, isSuccess, refetch };
 }
--- src/pages/Markets.tsx
+++ src/pages/Markets.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAllCryptoPrice } from "../features/markets/useAllCryptoPrice";
 import TopCryptoCard from "../features/markets/TopHotCryptoCard";
+import Spinner from "../ui/Spinner";
 
 export default function Markets() {
-  const { data, isSuccess } = useAllCryptoPrice();
+  const { data, isSuccess: isFetched, refetch } = useAllCryptoPrice(2000);
+
+  useEffect(
+    function () {
+      setInterval(refetch, 3000);
+    },
+    [refetch]
+  );
   // const data = getCryptoPrice("dogecoin");
   console.log(data);
   return (
-    <div className="flex justify-center items-center">
-      <TopCryptoCard />
+    <div className="flex justify-center items-start w-full ">
+      {isFetched ? (
+        <div className="flex gap-3 flex-wrap justify-center items-start">
+          <TopCryptoCard type="hot24"/>
+          <TopCryptoCard type="top24"/>
+          <TopCryptoCard type="big24"/>
+          {/* <TopCryptoCard /> */}
+          {/* <TopCryptoCard /> */}
+          {/* <TopCryptoCard /> */}
+        </div>
+      ) : (
+        <Spinner type="full" />
+      )}
     </div>
   );
 }
