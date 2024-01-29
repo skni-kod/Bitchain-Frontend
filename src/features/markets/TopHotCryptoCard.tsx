1ad005bde882af01c53bfe0c63f9d2b6e67514f2
Lukasz <7lukasz8@gmail.com>
2024-01-29 23:31:23 +0100

    top crypto cards component

--- src/features/markets/TopHotCryptoCard.tsx
+++ src/features/markets/TopHotCryptoCard.tsx
@@ -2,7 +2,6 @@ import { useQueryClient } from "@tanstack/react-query";
 import { useEffect, useRef } from "react";
 import { useSpecificCryptoInfo } from "./useSpecificCryptoInfo";
 import { formatCurrency } from "../../utils/helpers";
-import Spinner from "../../ui/Spinner";
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from "highcharts";
 
@@ -32,7 +31,11 @@ interface CryptoInfoData {
   timestamp: number;
 }
 
-export default function TopHotCryptoCard() {
+interface TopHotCryptoCardProps {
+  type: "hot24" | "top24" | "big24";
+}
+
+export default function TopHotCryptoCard({ type }: TopHotCryptoCardProps) {
   const {
     getSpecificCryptoInfo,
     isSuccess,
@@ -40,9 +43,18 @@ export default function TopHotCryptoCard() {
   } = useSpecificCryptoInfo();
   const queryClient = useQueryClient();
   const data: data = queryClient.getQueryData(["cryptoPrice"])!;
+  let hot24Data: CryptoInfoData | undefined;
+  if (type === "hot24") {
+    hot24Data = queryClient.getQueryData(["hot24"]);
+  } else if (type === "top24") {
+    hot24Data = queryClient.getQueryData(["top24"]);
+  } else if (type === "big24") {
+    hot24Data = queryClient.getQueryData(["big24"]);
+  }
 
   const topCrypto = useRef<CryptoData>();
   const options = useRef<unknown>();
+  const firstFetch = useRef<CryptoData>();
 
   console.log(topCrypto);
   console.log(topCryptoPrices);
@@ -50,126 +62,147 @@ export default function TopHotCryptoCard() {
   useEffect(() => {
     if (data) {
       const today = new Date().getTime();
+      let popular;
       const twentyFourHoursAgo = today - 24 * 3600000;
-      const popular = data.data.slice(0, 15);
-      topCrypto.current = popular.reduce((prevCrypto, currentCrypto) => {
+      if (type === "hot24") {
+        popular = data.data.slice(0, 15);
+      } else if (type === "top24") {
+        popular = data.data.slice(0, 2000);
+      } else if (type === "big24") {
+        popular = data.data.slice(0, 3);
+      }
+      topCrypto.current = popular?.reduce((prevCrypto, currentCrypto) => {
         return prevCrypto?.changePercent24Hr > currentCrypto?.changePercent24Hr
           ? prevCrypto
           : currentCrypto;
       }, popular[0]);
-      getSpecificCryptoInfo({
-        id: topCrypto.current.id,
-        interval: "m15",
-        start: twentyFourHoursAgo,
-        end: today,
-      });
+      if (!firstFetch.current?.priceUsd) {
+        getSpecificCryptoInfo({
+          id: topCrypto.current!.id!,
+          interval: "m15",
+          start: twentyFourHoursAgo,
+          end: today,
+        });
+        firstFetch.current = topCrypto.current;
+      }
+      if (type === "hot24") {
+        queryClient.setQueryData(["hot24"], topCryptoPrices);
+      } else if (type === "top24") {
+        queryClient.setQueryData(["top24"], topCryptoPrices);
+      } else if (type === "big24") {
+        queryClient.setQueryData(["big24"], topCryptoPrices);
+      }
     }
-  }, [data, getSpecificCryptoInfo]);
+  }, [data, getSpecificCryptoInfo, topCryptoPrices, queryClient, type]);
 
   useEffect(() => {
-    if (topCryptoPrices) {
-      const cryptoData = topCryptoPrices as CryptoInfoData;
-      const series = cryptoData?.data.map((item) => [
-        item.time,
-        +item.priceUsd,
-      ]);
-
-      options.current = {
-        chart: {
-          type: "line",
-          width: 120,
-          height: 80,
-          backgroundColor: "rgba(0, 0, 0, 0)",
-        },
-        plotOptions: {
-          series: {
-            color: "#ff5700",
-            enableMouseTracking: false,
-            marker: {
-              enabled: false, // wyłącz wyświetlanie markerów na liniach
-            },
-            dataLabels: {
-              enabled: false, // wyłącz wyświetlanie etykiet danych
-            },
-            legend: {
-              enabled: false, // wyłącz wyświetlanie legendy
-            },
-          },
-        },
-        credits: { enabled: false },
-        tooltip: {
-          enabled: false,
-          track: null
-        },
-        title: {
-          text: null, // ukryj tytuł wykresu
-        },
-        legend: {
-          enabled: false, // ukryj legendę
-        },
-        xAxis: {
-          visible: false, // ukryj osie x
-        },
-        yAxis: {
-          visible: false, // ukryj osie y
-        },
-        series: [
-          {
-            data: series,
-            name: "Crypto Prices", // Dodaj nazwę serii danych
-          },
-        ],
-      };
-      console.log(options.current);
+    if (!hot24Data) {
+      return;
     }
-  }, [topCryptoPrices, isSuccess]);
+    const cryptoData = hot24Data;
+    const series = cryptoData?.data.map((item) => [item.time, +item.priceUsd]);
+
+    options.current = {
+      chart: {
+        type: "line",
+        width: 120,
+        height: 80,
+        backgroundColor: "rgba(0, 0, 0, 0)",
+      },
+      plotOptions: {
+        series: {
+          color: "#ff5700",
+          enableMouseTracking: false,
+          marker: {
+            enabled: false, // wyłącz wyświetlanie markerów na liniach
+          },
+          dataLabels: {
+            enabled: false, // wyłącz wyświetlanie etykiet danych
+          },
+          legend: {
+            enabled: false, // wyłącz wyświetlanie legendy
+          },
+        },
+      },
+      credits: { enabled: false },
+      tooltip: {
+        enabled: false,
+        track: null,
+      },
+      title: {
+        text: null, // ukryj tytuł wykresu
+      },
+      legend: {
+        enabled: false, // ukryj legendę
+      },
+      xAxis: {
+        visible: false, // ukryj osie x
+      },
+      yAxis: {
+        visible: false, // ukryj osie y
+      },
+      series: [
+        {
+          data: series,
+          name: "Crypto Prices", // Dodaj nazwę serii danych
+        },
+      ],
+    };
+    console.log(options.current);
+  }, [hot24Data]);
 
   return (
     <div className="dark:bg-bgDark1 bg-bgWhite1Hover p-4 rounded-lg text-bgDark dark:text-bgWhite ">
       <p className="border-b-[1px] border-solid border-slate-200 dark:border-stone-700 text-xs px-2 pb-2">
-        24h hot
+        {type === "hot24"
+          ? "Hot 24"
+          : type === "top24"
+          ? "Top gainer 24"
+          : type === "big24" && "Top 5 Crypto"}
       </p>
-      {isSuccess && options.current ? (
-        <>
-          <div className="flex p-2 gap-2">
-            <img
-              className="rounded-full w-6"
-              src={`https://assets.coincap.io/assets/icons/${topCrypto.current?.symbol?.toLocaleLowerCase()}@2x.png`}
-            />
-            {topCrypto.current?.symbol}/USDT
-          </div>
-          <div className="flex gap-7">
-            <div>
-              <p className="font-bold px-2 pt-1">
-                {formatCurrency(+topCrypto.current!.priceUsd!)}
-              </p>
-              <p className=" text-[10px] text-gray px-2">
-                {formatCurrency(+topCrypto.current!.priceUsd!)} USD
-              </p>
-              <p
-                className={`px-2 text-xs pt-1 ${
-                  +topCrypto.current!.changePercent24Hr! > 0
-                    ? "text-green-500"
-                    : "text-red-500"
-                }`}
-              >
-                {+topCrypto.current!.changePercent24Hr! > 0
+      {/* {isSuccess ? ( */}
+      <>
+        <div className="flex p-2 gap-2">
+          <img
+            className="rounded-full w-6"
+            src={`https://assets.coincap.io/assets/icons/${topCrypto.current?.symbol?.toLocaleLowerCase()}@2x.png`}
+          />
+          {topCrypto.current?.symbol}/USDT
+        </div>
+        <div className="flex gap-7">
+          <div>
+            <p className="font-bold px-2 pt-1">
+              {topCrypto.current?.priceUsd &&
+                formatCurrency(+topCrypto.current!.priceUsd!)}
+            </p>
+            <p className=" text-[10px] text-gray px-2">
+              {topCrypto.current?.priceUsd &&
+                formatCurrency(+topCrypto.current!.priceUsd!)}{" "}
+              USD
+            </p>
+            <p
+              className={`px-2 text-xs pt-1 ${
+                topCrypto.current?.priceUsd &&
+                +topCrypto.current!.changePercent24Hr! > 0
+                  ? "text-green-500"
+                  : "text-red-500"
+              }`}
+            >
+              {topCrypto.current?.priceUsd &&
+                (+topCrypto.current!.changePercent24Hr! > 0
                   ? `+${formatCurrency(+topCrypto.current!.changePercent24Hr!)}`
-                  : formatCurrency(+topCrypto.current!.changePercent24Hr!)}
-                %
-              </p>
-            </div>
-            <div>
-              <HighchartsReact
-                options={options.current}
-                highcharts={Highcharts}
-              ></HighchartsReact>
-            </div>
+                  : formatCurrency(+topCrypto.current!.changePercent24Hr!))}
+              %
+            </p>
           </div>
-        </>
-      ) : (
-        <Spinner type="full" />
-      )}
+          <div>
+            <HighchartsReact
+              options={options.current}
+              highcharts={Highcharts}
+            ></HighchartsReact>
+          </div>
+        </div>
+      </>
     </div>
   );
 }
