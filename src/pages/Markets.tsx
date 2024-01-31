5cc2616acbe2ea6237035edfa6f64acf8e97e104
Lukasz <7lukasz8@gmail.com>
2024-01-31 23:48:02 +0100

    top crypto cards changes

--- src/pages/Markets.tsx
+++ src/pages/Markets.tsx
@@ -4,7 +4,7 @@ import TopCryptoCard from "../features/markets/TopHotCryptoCard";
 import Spinner from "../ui/Spinner";
 
 export default function Markets() {
-  const { data, isSuccess: isFetched, refetch } = useAllCryptoPrice(2000);
+  const { data, isSuccess: isFetched, refetch } = useAllCryptoPrice(500);
 
   useEffect(
     function () {
@@ -19,7 +19,7 @@ export default function Markets() {
       {isFetched ? (
         <>
           <div className="flex flex-col">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-8 w-full ">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 px-8 w-full ">
               <TopCryptoCard type="hot24" />
               <TopCryptoCard type="top24" />
               <TopCryptoCard type="big24" />
