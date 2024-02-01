da09d34feb08751bd4b235d7ed197f707804b463
Lukasz <7lukasz8@gmail.com>
2024-02-01 19:33:08 +0100

    user can finally choose his own currency

--- src/ui/navigation/SelectCurrency.tsx
+++ src/ui/navigation/SelectCurrency.tsx
@@ -3,8 +3,41 @@ import Modal from "../Modal";
 import IconButton from "../IconButton";
 import { SlGlobe } from "react-icons/sl";
 import SelectCurrenciesWindows from "./SelectCurrenciesWindows";
+import { useRates } from "../../features/markets/useRates";
+import { useQueryClient } from "@tanstack/react-query";
+import { useCallback, useEffect } from "react";
 
 export default function SelectCurrency() {
+  const { getRate, rate, isSuccess } = useRates();
+  const queryClient = useQueryClient();
+
+  const fetchData = useCallback(async () => {
+    getRate("tether");
+  }, [getRate]);
+
+  useEffect(
+    function () {
+      fetchData();
+    },
+    [fetchData]
+  );
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 20000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [fetchData]);
+
+  useEffect(() => {
+    if (isSuccess && rate) {
+      queryClient.setQueryData(["USDT"], rate.rateUsd);
+    }
+  }, [isSuccess, rate, queryClient]);
+
   return (
     <div>
       <Modal>
