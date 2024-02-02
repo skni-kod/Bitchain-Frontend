6b502520c011194d779d186a0a63e20709a8fdda
Lukasz <7lukasz8@gmail.com>
2024-02-02 19:39:36 +0100

    new helpers functions

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -5,3 +5,17 @@ export const formatCurrency = (value: number) => {
         return value.toFixed(2);
     }
 }
+
+export function formatBigNumbers(number) {
+    const suffixes = ['', 'K', 'M', 'B', 'T'];
+    const tier = Math.log10(Math.abs(number)) / 3 | 0;
+  
+    if (tier === 0) return number;
+  
+    const suffix = suffixes[tier];
+    const scale = Math.pow(10, tier * 3);
+  
+    const scaled = number / scale;
+  
+    return scaled.toFixed(2) + suffix;
+  }
