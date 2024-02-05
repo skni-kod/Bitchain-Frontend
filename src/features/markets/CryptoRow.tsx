6651d593f40479e5a44aed1f06c380ab5013a55e
Lukasz <7lukasz8@gmail.com>
2024-02-05 19:50:19 +0100

    fix crypto row responsive

--- src/features/markets/CryptoRow.tsx
+++ src/features/markets/CryptoRow.tsx
@@ -84,182 +84,106 @@ export default function CryptoRow({
         />
         {`${crypto.symbol}/USDT`}
       </div>
-      {width > 1024 ? (
-        <>
-          <div className="w-[130px]">
-            <p>
-              {crypto.priceUsd && usdtPrice
-                ? formatCurrency(+crypto.priceUsd * usdtPrice!)
-                : "--"}
-            </p>
-            <p className=" text-[10px] text-gray">
-              {crypto.priceUsd &&
-                (userCurrency
-                  ? formatCurrency(+crypto.priceUsd! / +userCurrency.rateUsd)
-                  : formatCurrency(+crypto.priceUsd!))}{" "}
-              {userCurrency ? userCurrency.symbol : "USD"}
-            </p>
-          </div>
-          <div className="w-[130px]">
-            <p
-              className={` ${
-                crypto.priceUsd && +crypto.changePercent24Hr! > 0
-                  ? "text-green-500"
-                  : "text-red-500"
-              }`}
-            >
-              {crypto.priceUsd &&
-                (+crypto.changePercent24Hr! > 0
-                  ? `+${formatCurrency(+crypto.changePercent24Hr!)}`
-                  : formatCurrency(+crypto.changePercent24Hr!))}
-              %
-            </p>
-          </div>
-          <div className="w-[130px]">
-            <p>{formatBigNumbers(crypto.volumeUsd24Hr)}</p>
-          </div>
-          <div className="w-[130px]">
-            <p>
-              {+crypto.marketCapUsd === 0
-                ? "--"
-                : formatBigNumbers(crypto.marketCapUsd)}
-            </p>
-          </div>
-          <div className="w-[130px] flex justify-center items-center gap-2 ml-3">
-            <Link
-              to={`/details/${crypto.name.replace(".", "-").toLowerCase()}/`}
-              className="p-2 text-xs text-main hover:text-mainHover "
-            >
-              Details
-            </Link>
-            <div>
-              <Button
-                id="basic-button"
-                aria-controls={open ? "basic-menu" : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
-                onClick={handleClick}
-                sx={{
-                  fontSize: "12px",
-                  textTransform: "none",
-                  backgroundColor: "#ff5700",
-                  color: "white",
-                  "&:hover": {
-                    backgroundColor: "#e84a00",
-                  },
-                }}
+      <p className="w-full lg:hidden"></p>
+      <div className="w-[130px] sm:mr-6 lg:mr-0 mr-0">
+        <p>
+          {crypto.priceUsd && usdtPrice
+            ? formatCurrency(+crypto.priceUsd * usdtPrice!)
+            : "--"}
+        </p>
+        <p className=" text-[10px] text-gray">
+          {crypto.priceUsd &&
+            (userCurrency
+              ? formatCurrency(+crypto.priceUsd! / +userCurrency.rateUsd)
+              : formatCurrency(+crypto.priceUsd!))}{" "}
+          {userCurrency ? userCurrency.symbol : "USD"}
+        </p>
+      </div>
+      <div className="w-[130px] hidden lg:block">
+        <p
+          className={` ${
+            crypto.priceUsd && +crypto.changePercent24Hr! > 0
+              ? "text-green-500"
+              : "text-red-500"
+          }`}
+        >
+          {crypto.priceUsd &&
+            (+crypto.changePercent24Hr! > 0
+              ? `+${formatCurrency(+crypto.changePercent24Hr!)}`
+              : formatCurrency(+crypto.changePercent24Hr!))}
+          %
+        </p>
+      </div>
+      <div className="w-[130px] hidden lg:block">
+        <p>{formatBigNumbers(crypto.volumeUsd24Hr)}</p>
+      </div>
+      <div className="w-[130px] hidden lg:block">
+        <p>
+          {+crypto.marketCapUsd === 0
+            ? "--"
+            : formatBigNumbers(crypto.marketCapUsd)}
+        </p>
+      </div>
+      <div className="w-[130px] justify-center items-center gap-2 ml-3 hidden sm:flex">
+        <Link
+          to={`/details/${crypto.name.replace(".", "-").toLowerCase()}/`}
+          className="p-2 text-xs text-main hover:text-mainHover "
+        >
+          Details
+        </Link>
+        <div>
+          <Button
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+            sx={{
+              fontSize: "12px",
+              textTransform: "none",
+              backgroundColor: "#ff5700",
+              color: "white",
+              "&:hover": {
+                backgroundColor: "#e84a00",
+              },
+            }}
+          >
+            Trade
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+              sx: {
+                backgroundColor: isDarkMode ? "#141519" : "#f1f5fc",
+                color: "white",
+              },
+            }}
+          >
+            <MenuItem onClick={handleClose}>
+              <Link
+                to={`/spot?${crypto.symbol}USDT`}
+                className="text-xs tracking-wider  transition-colors duration-300 text-bgDark dark:text-bgWhite hover:text-main dark:hover:text-main w-full h-full p-2"
               >
-                Trade
-              </Button>
-              <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                  "aria-labelledby": "basic-button",
-                  sx: {
-                    backgroundColor: isDarkMode ? "#141519" : "#f1f5fc",
-                    color: "white",
-                  },
-                }}
+                {`${crypto.symbol}/USDT`} pair at{" "}
+                <span className="font-bold">Spot</span>
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose} sx={{ padding: "none" }}>
+              <Link
+                to={`/futures?${crypto.symbol}USDT`}
+                className="text-xs tracking-wider transition-colors duration-300 text-bgDark dark:text-bgWhite hover:text-main dark:hover:text-main w-full h-full p-2"
               >
-                <MenuItem onClick={handleClose}>
-                  <Link
-                    to={`/spot?${crypto.symbol}USDT`}
-                    className="text-xs tracking-wider  transition-colors duration-300 text-bgDark dark:text-bgWhite hover:text-main dark:hover:text-main w-full h-full p-2"
-                  >
-                    {`${crypto.symbol}/USDT`} pair at{" "}
-                    <span className="font-bold">Spot</span>
-                  </Link>
-                </MenuItem>
-                <MenuItem onClick={handleClose} sx={{ padding: "none" }}>
-                  <Link
-                    to={`/futures?${crypto.symbol}USDT`}
-                    className="text-xs tracking-wider transition-colors duration-300 text-bgDark dark:text-bgWhite hover:text-main dark:hover:text-main w-full h-full p-2"
-                  >
-                    {`${crypto.symbol}/USDT`} pair at{" "}
-                    <span className="font-bold">Futures</span>
-                  </Link>
-                </MenuItem>
-              </Menu>
-            </div>
-          </div>
-        </>
-      ) : (
-        <div className="flex  ">
-          <div className="sm:w-[130px]">
-            <p>{formatCurrency(+crypto.priceUsd * usdtPrice!)}</p>
-            <p className=" text-[10px] text-gray">
-              {crypto.priceUsd &&
-                (userCurrency
-                  ? formatCurrency(+crypto.priceUsd! / +userCurrency.rateUsd)
-                  : formatCurrency(+crypto.priceUsd!))}{" "}
-              {userCurrency ? userCurrency.symbol : "USD"}
-            </p>
-          </div>
-          <div className="hidden sm:flex w-[130px]  justify-center items-center gap-2 ml-8">
-            <Link
-              to={`/details/${crypto.name.replace(".", "-").toLowerCase()}/`}
-              className="p-2 text-xs text-main hover:text-mainHover "
-            >
-              Details
-            </Link>
-            <div>
-              <Button
-                id="basic-button"
-                aria-controls={open ? "basic-menu" : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
-                onClick={handleClick}
-                sx={{
-                  fontSize: "12px",
-                  textTransform: "none",
-                  backgroundColor: "#ff5700",
-                  color: "white",
-                  "&:hover": {
-                    backgroundColor: "#e84a00",
-                  },
-                }}
-              >
-                Trade
-              </Button>
-              <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                  "aria-labelledby": "basic-button",
-                  sx: {
-                    backgroundColor: isDarkMode ? "#141519" : "#f1f5fc",
-                    color: "white",
-                  },
-                }}
-              >
-                <MenuItem onClick={handleClose}>
-                  <Link
-                    to={`/spot?${crypto.symbol}USDT`}
-                    className="text-xs tracking-wider  transition-colors duration-300 text-bgDark dark:text-bgWhite hover:text-main dark:hover:text-main w-full h-full p-2"
-                  >
-                    {`${crypto.symbol}/USDT`} pair at{" "}
-                    <span className="font-bold">Spot</span>
-                  </Link>
-                </MenuItem>
-                <MenuItem onClick={handleClose} sx={{ padding: "none" }}>
-                  <Link
-                    to={`/futures?${crypto.symbol}USDT`}
-                    className="text-xs tracking-wider transition-colors duration-300 text-bgDark dark:text-bgWhite hover:text-main dark:hover:text-main w-full h-full p-2"
-                  >
-                    {`${crypto.symbol}/USDT`} pair at{" "}
-                    <span className="font-bold">Futures</span>
-                  </Link>
-                </MenuItem>
-              </Menu>
-            </div>
-          </div>
+                {`${crypto.symbol}/USDT`} pair at{" "}
+                <span className="font-bold">Futures</span>
+              </Link>
+            </MenuItem>
+          </Menu>
         </div>
-      )}
+      </div>
     </div>
   );
 }
