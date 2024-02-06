bdc6ad6f922deae657c2bfd47d23ba37a4c54ab4
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-02-06 23:56:47 +0100

    idk i need to merge v2

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Outlet } from 'react-router-dom';
 import Navigation from './navigation/Navigation';
 
@@ -14,21 +13,4 @@ export default function AppLayout() {
 			</div>
 		</div>
 	);
-=======
-import { Outlet } from "react-router-dom";
-import Navigation from "./navigation/Navigation";
-
-export default function AppLayout() {
-  return (
-    <div
-      id="app"
-      className="grid grid-rows-[auto_1fr_auto] absolute h-full w-full top-0 left-0 overflow-x-hidden bg-white dark:bg-bgDark"
-    >
-      <Navigation />
-      <div className="overflow-auto">
-        <Outlet />
-      </div>
-    </div>
-  );
->>>>>>> 278eea702a312ee03717d0f8946033506213184e
 }
