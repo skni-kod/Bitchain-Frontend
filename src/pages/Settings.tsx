788bbce0639246117945f83f39e235821cc6527f
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-02-07 21:16:43 +0100

    ContentContainer

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import ContentContainer from '../ui/ContentContainer';
 
 export default function Settings() {
-  return (
-    <div>Settings</div>
-  )
+	return (
+		<ContentContainer>
+			<div className='flex mx-auto max-w-7xl flex-col h-full items-center'></div>
+      <button className='bg-green-600  px-4 py-2 rounded-full '>Click me</button>
+		</ContentContainer>
+	);
 }
