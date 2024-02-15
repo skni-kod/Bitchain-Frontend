7ad531f2ba0034ee9414a4083276d3e2908455b3
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-02-16 00:14:57 +0100

    three installed and btc object added

---
+++ src/features/pageNotFound/Container.tsx
@@ -0,0 +1,12 @@
+import { Canvas } from '@react-three/fiber';
+import Experience from './Experience';
+
+function Container() {
+	return (
+		<Canvas>
+			<Experience></Experience>
+		</Canvas>
+	);
+}
+
+export default Container;
