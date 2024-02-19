0580fd26529fde6b3e64172e71217c19c7f6ec60
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-02-19 01:23:07 +0100

    demo version of 404 page ready

--- src/features/pageNotFound/ThreeContainer.tsx
+++ src/features/pageNotFound/ThreeContainer.tsx
@@ -1,26 +1,11 @@
-import { OrbitControls } from '@react-three/drei';
+import { Html, OrbitControls } from '@react-three/drei';
 // import { Bitcoin } from './Bitcoin';
 import { Bitcoinv2 } from './Bitcoinv2';
 
 function ThreeContainer() {
 	return (
 		<>
-			<group position={[0, -1, 0]}>
-				<directionalLight position={[0, 1, 2]} intensity={10} />
-				<directionalLight position={[0, 1, -2]} intensity={10} />
-				<directionalLight position={[0, -1, 0]} intensity={10} />
-				<pointLight position={[1, 2, 1]} intensity={1} />
-				<pointLight position={[-1, 2, 1]} intensity={1} />
-				<pointLight position={[1, 2, -1]} intensity={1} />
-				<pointLight position={[-1, 2, 1]} intensity={1} />
-
-				<ambientLight intensity={1} />
-				<OrbitControls enableZoom={true} enableRotate={true} />
-				<group>
-					{/* <Bitcoin scale={0.25} /> */}
-					<Bitcoinv2 scale={0.20} />
-				</group>
-			</group>
+			
 		</>
 	);
 }
