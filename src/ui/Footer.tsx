20595dfaf65bd7c722c15352550c3ef34c89adc4
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-03-01 18:29:50 +0100

    Simple Footer v1

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,31 +1,38 @@
-import { Link } from "react-router-dom";
-import useDarkMode from "../hooks/useDarkMode";
-import { GoDotFill } from "react-icons/go";
+import { Link } from 'react-router-dom';
+import useDarkMode from '../hooks/useDarkMode';
+import { GoDotFill } from 'react-icons/go';
+import { AiOutlineClose } from 'react-icons/ai';
 
 function Footer() {
-  const date = new Date().getFullYear();
-  const { isDarkMode } = useDarkMode();
+	const date = new Date().getFullYear();
+	const { isDarkMode } = useDarkMode();
 
-  return (
-    <footer className="py-8 text-gray flex justify-center items-center gap-6 dark:bg-bgDark max-w-7xl">
-      <div className="flex gap-2 justify-center items-center">
-        <img className="h-5" src="/logo-icon-gray.svg" alt="" />
-        <p>{date} BitChain Project</p>
-      </div>
-      <Link
-        to="https://kod.prz.edu.pl/"
-        className="hover:text-main transition-colors duration-300"
-      >
-        Skni kod
-      </Link>
-      <Link
-        to="/about"
-        className="hover:text-main transition-colors duration-300"
-      >
-        About Us
-      </Link>
-    </footer>
-  );
+	return (
+		<footer className='relative mt-16 pt-6 pb-8 px-10 mx-auto flex justify-center flex-wrap  dark:bg-bgDark max-w-7xl text-gray'>
+      <div className='absolute w-5/6 h-[1px] left-1/2 -translate-x-1/2 -top-5 bg-main'></div>
+			<div className='flex flex-col items-center gap-5 w-full'>
+				<img
+					src={isDarkMode ? '/logo-skni-black.png' : '/logo-skni-white.png'}
+					alt='logo Skni KOD'
+					className='w-32 '
+				/>
+				<span className='text-main text-xl'>
+					<AiOutlineClose />
+				</span>
+				<img
+					src={isDarkMode ? '/logo-black.png' : '/logo-white.png'}
+					alt='logo BitChain'
+					className='w-32'
+				/>
+			</div>
+			<div className='relative mt-16'>
+				<div className='absolute w-5/6 h-[1px] left-1/2 -translate-x-1/2 -top-6 bg-main'></div>
+				<p className='text-center '>
+					All Rights Reserved &copy; {date} BitChain{' '}
+				</p>
+			</div>
+		</footer>
+	);
 }
 
 export default Footer;
