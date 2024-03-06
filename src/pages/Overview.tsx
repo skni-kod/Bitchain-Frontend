b30d6e675c8d85579a2fafc49577c07be9d50ca0
Lukasz <7lukasz8@gmail.com>
2024-03-06 23:50:38 +0100

    wallet nav

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import WalletContent from "../features/Wallets/WalletContent";
+import WalletNav from "../features/Wallets/WalletNav";
 
 export default function Overview() {
   return (
-    <div>Overview</div>
-  )
+    <div>
+      <WalletNav />
+      <WalletContent />
+    </div>
+  );
 }
