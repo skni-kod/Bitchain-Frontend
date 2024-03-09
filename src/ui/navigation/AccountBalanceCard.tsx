c4cbb6b6e6cc245e4fd81bada7eee5bdb7853814
Lukasz <7lukasz8@gmail.com>
2024-03-09 21:20:53 +0100

    account balance card component

--- src/ui/navigation/AccountBalanceCard.tsx
+++ src/ui/navigation/AccountBalanceCard.tsx
@@ -1,27 +1,64 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 import { IoIosArrowForward } from "react-icons/io";
+import { useQueryClient } from "@tanstack/react-query";
+import { userCurrency } from "../cryptoDetails/DetailsHeader";
+import { formatCurrency } from "../../utils/helpers";
 
 interface AccountBalanceCardProps {
-  onCloseFunction: () => void;
+  onCloseFunction?: () => void;
+  assetsUsd: number;
+  type: "nav" | "account";
 }
 
 export default function AccountBalanceCard({
   onCloseFunction,
+  assetsUsd,
+  type,
 }: AccountBalanceCardProps) {
-  const userData = { balance: 14232.312, km: 12321.212 }; //Pablo kiedy logowanie??????????????????????????????????????????????????///
+  const queryClient = useQueryClient();
+  const userCurrency: userCurrency | undefined = queryClient.getQueryData([
+    "userCurrency",
+  ]);
+  const usdt: string | undefined = queryClient.getQueryData(["USDT"]);
 
   return (
-    <div className="flex flex-col w-full p-3 bg-bgWhite1Hover dark:bg-bgDark1Hover rounded-lg mb-3 mt-2 cursor-default">
-      <NavLink to="/overview?tab=1" className="flex justify-between items-center w-full" onClick={onCloseFunction}>
-        <p>Total assets</p>
-        <span>
-          <IoIosArrowForward />
-        </span>
-      </NavLink>
-      <span className="text-2xl text-left my-2">{userData.balance} USDT</span>
-      <span className="text-xs text-left font-normal text-slate-300">
-        ~ KM {userData.km}
+    <div
+      className={`flex flex-col w-full  bg-bgWhite1Hover dark:bg-bgDark1Hover text-bgDark dark:text-bgWhite rounded-lg  cursor-default ${
+        type === "nav" ? "p-3 mb-3 mt-2" : "p-7 my-8"
+      }`}
+    >
+      {type === "nav" ? (
+        <NavLink
+          to="/overview?tab=1"
+          className="flex justify-between items-center w-full"
+          onClick={onCloseFunction}
+        >
+          <p>Total assets</p>
+          <span>
+            <IoIosArrowForward />
+          </span>
+        </NavLink>
+      ) : (
+        <div>
+          <p className="text-slate-500 dark:text-slate-300">Total assets</p>
+        </div>
+      )}
+      <span
+        className={`font-medium text-left my-2 ${
+          type === "nav" ? "text-2xl" : "text-[32px]"
+        }`}
+      >
+        {usdt ? formatCurrency(assetsUsd * +usdt) : "--.--"} USDT
+      </span>
+      <span
+        className={`text-left font-normal text-slate-300 ${
+          type === "nav" ? "text-xs" : "text-base text-slate-500 dark:text-slate-300"
+        }`}
+      >
+        ~ {userCurrency ? userCurrency.currencySymbol : "$"}
+        {userCurrency
+          ? formatCurrency(assetsUsd / userCurrency.rateUsd)
+          : formatCurrency(assetsUsd)}
       </span>
     </div>
   );
