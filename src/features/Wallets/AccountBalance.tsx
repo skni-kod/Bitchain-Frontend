ecf9c4d64fd286fbd86c04ba33d0b8e2a80ef0e7
Lukasz <7lukasz8@gmail.com>
2024-03-10 22:00:32 +0100

    account details

---
+++ src/features/Wallets/AccountBalance.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { IoIosArrowForward } from "react-icons/io";
+import { useSearchParams } from "react-router-dom";
+import { formatCurrency } from "../../utils/helpers";
+
+interface AccountBalanceProps {
+  title: string;
+  balance: number;
+  balanceDefCur: number;
+  to: string;
+  defSymbol: string;
+}
+
+export default function AccountBalance({
+  balance,
+  balanceDefCur,
+  title,
+  to,
+  defSymbol,
+}: AccountBalanceProps) {
+  const [searchParms, setSearchParams] = useSearchParams();
+
+  function onClick() {
+    searchParms.set("tab", to);
+    setSearchParams(searchParms);
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      className="flex justify-between items-center py-10 w-full"
+    >
+      <div className="flex flex-col gap-3 text-left">
+        <p className="text-sm">{title}</p>
+        <p className="text-xs text-gray">
+          <span className="text-[20px] font-semibold text-bgDark dark:text-bgWhite">
+            {formatCurrency(balance)} USDT
+          </span>{" "}
+          ~ {defSymbol}
+          {formatCurrency(balanceDefCur)}
+        </p>
+      </div>
+      <span className="text-xl">
+        <IoIosArrowForward />
+      </span>
+    </button>
+  );
+}
---
+++ src/features/Wallets/AccountDetail.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import AccountBalance from "./AccountBalance";
+
+export default function AccountDetail() {
+  return (
+    <div>
+      <p className="text-[20px] font-semibold pt-16 pb-6">Account Details</p>
+      <div className="w-full bg-slate-100 h-[1px]"></div>
+      <div>
+        <AccountBalance
+          title="Fund Account"
+          balance={153}
+          defSymbol="$"
+          balanceDefCur={32132}
+          to="1"
+        />
+        <div className="w-full bg-slate-100 h-[1px]"></div>
+        <AccountBalance
+          title="Futures Account"
+          balance={153}
+          defSymbol="$"
+          balanceDefCur={32132}
+          to="2"
+        />
+        <div className="w-full bg-slate-100 h-[1px]"></div>
+        <AccountBalance
+          title="Stacking Account"
+          balance={153}
+          defSymbol="$"
+          balanceDefCur={32132}
+          to="3"
+        />
+        <div className="w-full bg-slate-100 h-[1px]"></div>
+      </div>
+    </div>
+  );
+}
